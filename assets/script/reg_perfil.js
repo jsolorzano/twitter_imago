be35501db40686679d50619a7aec0c59386b775e
Jose <solorzano202009@gmail.com>
2017-11-10 14:57:00 -0400

    Registro y asociación de perfiles a una cuenta de twitter.

---
+++ assets/script/reg_perfil.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+	// Capturamos la base_url
+    var base_url = $("#base_url").val();
+	
+	$('input').on({
+        keypress: function () {
+            $(this).parent('div').removeClass('has-error');
+        }
+    });
+
+    $('#volver').click(function () {
+        url = base_url+'twitters/view?id='+$("#id_twitter").val()+'&screen_name=&ruta='+$("#ruta_origen").val();
+        window.location = url;
+    });
+    
+    // Cargamos los datos de la persona usando la api de BVA
+    $.get('http://consultaelectoral.bva.org.ve/cedula='+$("#cedula").val().trim()+'?format=json', function (response) {  // Se produce un error si usamos $.post en vez de $.get
+		
+		//~ alert(response['USD']['transferencia']);
+		var cedula = response[0]['cedula'];
+		var genero = response[0]['sexo'];
+		var nombre = response[0]['p_nombre'] + ' ' + response[0]['s_nombre'];
+		var apellido = response[0]['p_apellido'] + ' ' + response[0]['s_apellido'];
+		var fecha_nac = response[0]['f_nac'];
+		var centro_votacion = response[0]['c_votacion'];
+		var direccion_centro_votacion = response[0]['c_direccion'];
+		$("#genero").val(genero);
+		$("#nombre").val(nombre);
+		$("#apellido").val(apellido);
+		$("#fecha_nac").val(fecha_nac);
+		$("#centro_votacion").val(centro_votacion);
+		$("#direccion_centro_votacion").val(direccion_centro_votacion);
+		
+	}, 'json');
+	
+
+    $("#registrar").click(function (e) {
+
+        e.preventDefault();  // Para evitar que se envíe por defecto
+
+        if ($('#cedula').val().trim() === "") {
+			swal("Disculpe,", "para continuar debe ingresar nombre");
+			$('#name').parent('div').addClass('has-error');
+			
+        } else {
+
+            $.post(base_url+'CPerfilSocial/add', $('#form_perfil_social').serialize(), function (response) {
+
+				if (response == 'existe') {
+                    swal("Disculpe,", "esta cédula se encuentra registrada");
+                }else{
+					swal({ 
+						title: "Registro",
+						text: "Guardado con exito",
+						type: "success" 
+					},
+					function(){
+						url = base_url+'twitters/view?id='+$("#id_twitter").val()+'&screen_name=&ruta='+$("#ruta_origen").val();
+						window.location = url;
+					});
+
+				}
+
+            });
+            
+        }
+    });
+	
+});
--- assets/script/twitter_view.js
+++ assets/script/twitter_view.js
@@ -1,7 +1,34 @@
 $(document).ready(function(){
+	
+	$('.footable').footable();  // Aplicamos el plugin footable
+	
 	// Capturamos la base_url
     var base_url = $("#base_url").val();
 	
+	
+	// Función para ver el perfil de twitter
+	$("#boton_vincular").on('click', function (e) {
+		
+		swal({
+            title: "Crear y vincular perfil",
+            text: "¿Está seguro de crear un nuevo perfil y vincularlo a esta cuenta?",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Crear",
+            cancelButtonText: "Cancelar",
+            closeOnConfirm: true,
+            closeOnCancel: true
+        },
+        function(isConfirm){
+            if (isConfirm) {
+             
+                $("#modal_cedula").modal('show');
+                
+            }
+        });
+		
+	});
 	
 	// Función para ver el perfil de twitter
 	$("#vincular").on('click', function (e) {
@@ -13,7 +40,7 @@ $(document).ready(function(){
 			
 		}else{
 			
-			window.location.href = base_url+'twitters/view?id='+$("#id").val()+'&screen_name='+$("#screen_name").val();
+			window.location.href = base_url+'social_profile/register?id_twitter='+$("#id_twitter").val()+'&cedula='+$("#cedula").val().trim()+'&ruta='+$("#ruta_origen").val();
 		
 		}
 		
